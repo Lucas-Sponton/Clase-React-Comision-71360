b265d286fd092343f643fb5cef81b4706af1b4d5
Lucas Sponton <lucas.sponton6@gmail.com>
2024-07-28 17:30:58 -0300

    Trabaje un poco el proyecto y arranque mi css

--- curso-react/src/componentes/header/ItemListContainer.jsx
+++ curso-react/src/componentes/header/ItemListContainer.jsx
@@ -3,6 +3,7 @@ import arrayProductos from "../../assets/json/productos.json"
 import { useEffect, useState } from "react"
 import ItemList from "../ItemList"
 import { useParams } from "react-router-dom"
+//import { collection, doc, getDoc, getDocs, getFirestore } from "firebase/firestore";
 
 
 const ItemListContainer = () => {
@@ -21,6 +22,25 @@ const ItemListContainer = () => {
         })
     }, [id])
 
+    /*useEffect(() => {
+        const db = getFirestore();
+        const docRef = doc(db, "Items", "XTBs092lQcKWfmCPnVNX");
+        getDoc(docRef).then(documento => {
+            if (documento.exists()) {
+                setItems({id:documento.id, ...documento.data})
+            } else {
+                console.error("Error! No existe el documento!");
+            }
+        })
+    }, [])*/
+
+    /*useEffect(() => {
+        const db = getFirestore();
+        const itemsCollection = collection(db, "Items");
+        getDocs(itemsCollection).then(snapShot => {
+            setItems(snapShot.docs.map(documento => ({id: documento.id, ...documento.data()})))
+        })
+    }, [])*/
 
     return (
         <div className="container">
--- curso-react/src/main.jsx
+++ curso-react/src/main.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAQgKsq9nKLkYXFtVxngV0rHF2B4rYoOd8",
+    authDomain: "coderhouse-reactjs-1.firebaseapp.com",
+    projectId: "coderhouse-reactjs-1",
+    storageBucket: "coderhouse-reactjs-1.appspot.com",
+    messagingSenderId: "407216632225",
+    appId: "1:407216632225:web:3b458ad86b8c7ac7721be0"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
