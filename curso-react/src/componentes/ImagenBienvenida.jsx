837bd3a0c3e2705c17c3a569f12beb6b38facd38
Lucas Sponton <lucas.sponton6@gmail.com>
2024-08-10 13:50:02 -0300

    creo que encontre el problema de mi deploy, puede haya sido error con toastify.

--- curso-react/src/componentes/ImagenBienvenida.jsx
+++ curso-react/src/componentes/ImagenBienvenida.jsx
@@ -1,7 +1,7 @@
 const ImagenBienvenida = () => {
     return (
         <div className="col-md-12 mt-2 mb-3">
-            <img src="../public/images/cafeteria-4.webp" alt="¡Cafeteria bienvenida!" className="my-2 rounded-5 rounded-pill" style={{ width: 100 + '%', height: 'auto' }} />
+            <img src="../images/cafeteria-4.webp" alt="¡Cafeteria bienvenida!" className="my-2 rounded-5 rounded-pill" style={{ width: 100 + '%', height: 'auto' }} />
         </div>
     )
 }
